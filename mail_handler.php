43b93b7db6f5029f54f20e01b05b43da60fc8a52
Robert Sęk <robert.sek@powel.com>
2018-03-05 21:27:41 +0100

    php emails

--- mail_handler.php
+++ mail_handler.php
@@ -1,5 +1,5 @@
 <?php 
-if(isset($_POST['submit'])){
+if(isset($_POST['btn-submit'])){
     $to = "info@att-deutschland.de";
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['name'];
@@ -7,12 +7,27 @@ if(isset($_POST['submit'])){
     $subject = "Formularübermittlung";
     $subject2 = "Kopie der Formularvorlage";
     $message = $first_name . " " . $number . " hat geschriben:" . "\n\n" . $_POST['message'];
-    $message2 = "Kopie der Formularvorlage: " . $first_name . "\n\n" . $_POST['message'];
+    $message2 = "Kopie der Formularvorlage: ". $first_name . " " . $number . " " . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
+    $success = mail($to,$subject,$message,$headers2);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "E-Mail gesendet. Vielen Dank" . $first_name . ", wir werden Sie in Kürze kontaktieren.";
     }
 ?>
+<body>
+    <?php if (isset($success) && $success) { ?>
+        <script>
+            window.location.href = "https://att-deutschland.de";
+            alert('E-Mail gesendet. Vielen Dank, wir werden Sie in Kürze kontaktieren.');
+        </script>
+    <?} else { ?>
+        <script>
+            window.history.back(-1);
+            alert('Error');
+        </script>
+    <?} ?>
+</body>
+
+
+
